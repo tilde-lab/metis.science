860b672112daf2921971db362ec7857ff664c7ec
blokhin <eb@tilde.pro>
2022-12-15 15:18:01 +0000

    Deploying to gh-pages from @ tilde-lab/metis.science@af9f7d4c0a88a7373a41c0df189757c08e2f86ab 🚀

---
+++ src/views/Footer.svelte
@@ -0,0 +1,51 @@
+<footer class="py-2 mt-2">
+	<Container>
+		<Grid>
+			<Col>
+			<span class="py-2" style="line-height: 2em;">
+				Developed by <a href="https://tilde.pro" target="_blank">Tilde MI</a> for <a href="https://www.basf.com" target="_blank">BASF SE</a> with the BSD open-source license.
+			</span>
+			</Col>
+			<Col col="auto">
+				<Switch bind:value={$darkTheme}>Dark theme</Switch>
+			</Col>
+		</Grid>
+	</Container>
+</footer>
+
+<script lang="ts" context="module">
+	import { Col, Grid, Container, Switch } from 'svelte-spectre';
+	import { nodeAttribute } from '@/helpers/dom';
+	import { darkTheme } from '@/stores/theme';
+</script>
+
+<script lang="ts">
+	$: nodeAttribute(document.documentElement, 'color-scheme', $darkTheme ? 'dark' : 'light');
+</script>
+
+<style lang="scss">
+	:global(body),
+	:global(html) {
+		height: 100%;
+		:global(.spectre) {
+			min-height: 100%;
+			display: grid !important;
+			grid-template-rows: auto 1fr auto;
+		}
+	}
+	@media (prefers-color-scheme: dark) {
+		footer {
+			background-color: #343a51;
+		}
+	}
+	:global([color-scheme='dark']) {
+		footer {
+			background-color: #343a51;
+		}
+	}
+	:global([color-scheme='light']) {
+		footer {
+			background-color: #f1f1fc;
+		}
+	}
+</style>
---
+++ src/views/Sliders/SliderModal.svelte
@@ -0,0 +1,20 @@
+<Modal bind:open size="lg">
+	{#key index}
+		<Slidy {slides} {index} snap="center" --slidy-slide-width="100%" let:item loop={true} navigation={false}>
+			<Figure width={item.width} height={item.height} contain src={item.src}>
+				{item.caption}
+			</Figure>
+		</Slidy>
+	{/key}
+</Modal>
+
+<script lang="ts" context="module">
+	import { Figure, Modal } from 'svelte-spectre';
+	import { Slidy, type Slide } from '@slidy/svelte';
+</script>
+
+<script lang="ts">
+	export let slides: Slide[] = [];
+	export let index: number = 3;
+	export let open = false;
+</script>
