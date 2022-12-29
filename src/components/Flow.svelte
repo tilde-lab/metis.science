757c6af6d4954cfbb16c3dbc5452c7c30427f0d8
Glisten**STAR** <glistenstar00@gmail.com>
2022-12-29 10:21:59 +0100

    add: flow component

---
+++ src/components/Flow.svelte
@@ -0,0 +1,68 @@
+<div class="flow">
+	<div class="flow_wrapper">
+		<div class="flow_item">
+			<span class="sign">#</span>	
+			<span>Metis is a digital materials lab assistant.</span>	
+		</div>
+		<div class="flow_item">
+			<span class="sign">#</span>	
+			<span>Metis is a digital materials lab assistant.</span>	
+		</div>
+		<div class="flow_item">
+			<span class="sign">#</span>	
+			<span>Metis is a digital materials lab assistant.</span>	
+		</div>
+		<div class="flow_item">
+			<span class="sign">#</span>	
+			<span>Metis is a digital materials lab assistant.</span>	
+		</div>
+	</div>
+</div>
+
+<script lang="ts" context="module">
+</script>
+
+<script lang="ts">
+</script>
+
+<style lang="scss">
+
+	.flow {
+		padding: 48px 0;
+		background: #111;
+		color: white;
+		border-bottom: 1px solid #adadad;
+		border-top: 1px solid #adadad;
+		overflow: hidden;
+		margin-top: -3px;
+		display: flex;
+	}
+	.flow_wrapper{
+		height: 100%;
+		width: 200vw;
+		display: flex;
+		animation: running-anim 10s forwards infinite linear;
+		@keyframes running-anim {
+            from {
+                transform: translateX(0)
+			}
+            to {
+                transform: translateX(-39.5vw)
+			}
+		}
+	}
+
+	.flow_item{
+		display: flex;
+		align-items: center;
+		margin-right: 180px;
+		width: max-content;
+	}
+	.flow_item span{
+		font-size: 28px;
+	}
+	.flow_item .sign{
+		font-size: 40px;
+		margin-right: 5px;
+	}
+</style>
--- src/layouts/Main.svelte
+++ src/layouts/Main.svelte
@@ -1,17 +1,23 @@
 <main>
 	<div class="columns">
 		<div class="column col-8">
-			<Hero />
+			<Hero>
+				<h1>Metis is your new</h1>
+				<h1 class="text-primary">virtual materials lab</h1>
+				<Button variant="secondary" size="xl">Download now</Button>
+			</Hero>
 		</div>
 		<div class="column col-4">
-			<img src={hero_image} alt="metis"/>
+			<img src="assets/img/hero.jpg" alt="img"/>
 		</div>
 	</div>
+
+	<Flow />
 </main>
 
 <script lang="ts">
-	import { Hero } from 'svelte-spectre'
-	import hero_image from "@/assets/img/hero.jpg"
+	import { Hero, Button } from 'svelte-spectre'
+	import { Flow } from '@/components'
 </script>
 
 <style>
@@ -21,5 +27,28 @@
 	}
 	.columns{
 		width: 100%;
+		height: calc(100vh - 143px);
+		align-items: center;
+		flex-wrap: nowrap;
+		margin: 0px;
+	}
+	.columns .col-8{
+		padding-left: 50px;
+	}
+	.col-8 h1{
+		font-size: 112px;
+		margin-bottom: 0px;
+	}
+	.col-8 h1.text-primary{
+		margin-bottom: 48px;
+	}
+
+	.col-4 {
+		padding: 0px;
+	}
+	.col-4 img{
+		width: 100%;
+		height: calc(100vh - 143px);
+		margin-bottom: 0px;
 	}
 </style>
